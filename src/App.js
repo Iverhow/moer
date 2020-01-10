83e05c54eeb9f2a22b302c099497e5b5befdd735
Simon Li <Simon_Li@huatek.com>
2020-01-11 00:38:51 +0800

    add redux case

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import { connect } from 'react-redux'
+// import { addGun, removeGun, addGunAsync,addTwice } from './index.redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 装饰器模式
+// @connect(
+//     state=>({ num: state}),
+//     {addGun, removeGun, addGunAsync,addTwice}
+// )
+class App extends React.Component{
+    render() {
+        // console.log(this.props)
+        // num addGun，removeGun，addGunAsync都是connect给的，不需要手动dispatch
+        const store = this.props.store;
+        const num = store.getState();
+        const addGun = this.props.addGun;
+        const removeGun = this.props.removeGun;
+        const addGunAsync = this.props.addGunAsync;
+        return (
+            <div>
+                <h2>现在有机枪{num}把</h2>
+                <button onClick={() => store.dispatch(addGun())}>申请武器</button>
+                <button onClick={() => store.dispatch(removeGun())}>上交武器</button>
+                <button onClick={() => store.dispatch(addGunAsync())}>拖两天再给</button>
+            </div>
+        )
+    }
 }
-
+// App =
+// connect(
+//   state=>({ num: state}),
+//   {addGun, removeGun, addGunAsync}
+// )
+// (App)
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
+import {counter, addGun, removeGun, addGunAsync} from './index.redux'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(counter, applyMiddleware(thunk));
+// ReactDOM.render(
+//     (
+//         <App store={store}
+//              addGun={addGun}
+//              removeGun={removeGun}
+//              addGunAsync={addGunAsync}/>
+//     ),
+//     document.getElementById('root'));
+function render() {
+    ReactDOM.render(
+        (
+            <App store={store}
+                 addGun={addGun}
+                 removeGun={removeGun}
+                 addGunAsync={addGunAsync}/>
+        ),
+        document.getElementById('root'));
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render();
+store.subscribe(render);
